1ade7839bf8054f15827e7616ba95c9c17c801ff
SaiBorg <saifulrohmat8@gmail.com>
2025-11-19 00:21:09 +0700

    Test Commit

---
+++ NumberReverseApp/Program.cs
@@ -0,0 +1,36 @@
+﻿string input;
+do
+{
+    Console.Write("Masukan Angka untuk direverse: ");
+    input = Console.ReadLine();
+    if (!input.All(x => (x >= 48 && x <= 57) || x == 44 || x == 46))
+    {
+        Console.WriteLine("Input Tidak Valid. Bisa dicoba kembali");
+        continue;
+    }
+    input = input.Replace(",", "").Replace(".", "");
+    var inputReverse = "";
+    for (int i = 1; i <= input.Length; i++)
+    {
+        inputReverse += input[input.Length - i];
+    }
+    var intInput = int.Parse(input);
+    var intInputReverse = int.Parse(inputReverse);
+
+    int Difference;
+    if (intInput > intInputReverse)
+    {
+        Difference = intInput - intInputReverse;
+    }
+    else
+    {
+        Difference = intInputReverse - intInput;
+    }
+    Console.WriteLine("====================Hasil===============");
+    Console.WriteLine($"Input : {intInput}");
+    Console.WriteLine($"Reverse : {intInputReverse}");
+    Console.WriteLine($"Difference : {Difference}");
+    Console.WriteLine("=========================================");
+
+} while (!string.IsNullOrEmpty(input));
+
